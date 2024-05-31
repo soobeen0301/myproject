5b54ac58095fa3f57031c3d13ad388524d9a9740
soobeen0301 <xg12041@gmail.com>
2024-05-31 19:59:50 +0900

    second commit

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ function sumNumber(a, b) {
 
 const sumResult = sumNumber(2, 1);
 console.log(sumResult);
+
+function multiplyNumber(a, b) {
+  return a * b;
+}
+
+const multiplyResult = multiplyNumber(3, 5);
+console.log(multiplyResult);
